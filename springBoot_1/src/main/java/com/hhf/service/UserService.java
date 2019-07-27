de9e30f1ba004cbe6057f6493f160fabc539a6c1
hhf <290071186@qq.com>
2019-07-27 14:56:03 +0800

    集成redis

--- springBoot_1/src/main/java/com/hhf/service/UserService.java
+++ springBoot_1/src/main/java/com/hhf/service/UserService.java
@@ -7,7 +7,11 @@ package com.hhf.service;
 
 import java.util.List;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +28,9 @@ public class UserService /*extends CommonDao*/{
 	
 	@Autowired
 	private UserMapper userMapper;
+
+	@Autowired
+	StringRedisTemplate stringRedisTemplate;
 	
 	@Transactional
 	public int insertUser(String userName,String passWord) {
@@ -35,9 +42,17 @@ public class UserService /*extends CommonDao*/{
 	
 	public PageInfo<User> query(String name,int pageNum,int pageSize) {
 		log.info("----------queryUserByPage:service-----------");
+		List<User> findByName=null;
 		//使用pagehelper:生产page信息
 		PageHelper.startPage(pageNum, pageSize);//底层：改写（拼接）sql
-		List<User> findByName = userMapper.findByName(name);
+		String usekey = stringRedisTemplate.opsForValue().get(name+"userkey");
+		if(null!= usekey){//redis缓存
+			findByName = JSONObject.parseArray(usekey, User.class);
+		}else{
+			 findByName = userMapper.findByName(name);
+			String value = JSON.toJSONString(findByName);
+			stringRedisTemplate.opsForValue().set(name+"userkey",value);
+		}
 		PageInfo<User> result=new PageInfo<User>(findByName);
 		return result;
 	}
@@ -47,5 +62,6 @@ public class UserService /*extends CommonDao*/{
 		log.info("----------queryUserByType:service-----------");
 		return userMapper.findByType(yes);
 	}
+
 	
 }
