f8c7d4029b10dcbb06ca0d2fde19f6a56105119e
hhf <290071186@qq.com>
2019-08-05 15:03:31 +0800

    优化redis操作

--- springBoot_1/src/main/java/com/hhf/service/UserService.java
+++ springBoot_1/src/main/java/com/hhf/service/UserService.java
@@ -6,12 +6,15 @@ package com.hhf.service;
  */
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,26 +45,62 @@ public class UserService /*extends CommonDao*/{
 	
 	public PageInfo<User> query(String name,int pageNum,int pageSize) {
 		log.info("----------queryUserByPage:service-----------");
-		List<User> findByName=null;
 		//使用pagehelper:生产page信息
 		PageHelper.startPage(pageNum, pageSize);//底层：改写（拼接）sql
-		String usekey = stringRedisTemplate.opsForValue().get(name+"userkey");
-		if(null!= usekey){//redis缓存
-			findByName = JSONObject.parseArray(usekey, User.class);
-		}else{
-			 findByName = userMapper.findByName(name);
-			String value = JSON.toJSONString(findByName);
-			stringRedisTemplate.opsForValue().set(name+"userkey",value);
-		}
+		List<User> findByName = JSONObject.parseArray(loadCacheRedis(name), User.class);
 		PageInfo<User> result=new PageInfo<User>(findByName);
 		return result;
 	}
-	
-	
+
+	/**
+	 * 读取redis缓存，使用redis分布式锁
+	 * @param name
+	 * @return
+	 */
+	private String loadCacheRedis(String name) {
+		String usekey = stringRedisTemplate.opsForValue().get(name);
+		if(null!= usekey){//redis缓存
+			log.info("读取redis缓存......");
+			return usekey;
+		}else{
+			//setIfAbsent,底层还是jedis的setNx
+			if(stringRedisTemplate.opsForValue().setIfAbsent("lock","up")){//拿到redis分布锁，去mysql查询
+				log.info("拿到锁了，去DB中查询...");
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				List<User> findByName = userMapper.findByName(name);
+				String value = JSON.toJSONString(findByName);
+				stringRedisTemplate.opsForValue().set(name,value);//放入Redis
+				stringRedisTemplate.expire(name,10, TimeUnit.SECONDS);//设置失效时间
+				stringRedisTemplate.delete("lock");//释放掉锁：遇到异常的时候，锁无法释放。
+				return value;
+			}else{//没拿到锁，休眠200ms，再去拿。防止大量sql同时怼到db。 造成，缓存击穿
+				try {
+					log.info("没有拿到锁，等待200ms...");
+					Thread.sleep(200);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				return loadCacheRedis(name);//递归调用
+			}
+		}
+	}
+
+
+	/**
+	 * redis集群
+	 * @param yes
+	 * @return
+	 */
 	public List<User> queryVIP(Integer yes) {
-		log.info("----------queryUserByType:service-----------");
+		stringRedisTemplate.opsForCluster();
 		return userMapper.findByType(yes);
 	}
+
+
 
 	
 }
