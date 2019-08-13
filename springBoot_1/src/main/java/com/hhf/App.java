4e2719014e4d0125ae1c4f608beb6df2fd84960b
hhf <290071186@qq.com>
2019-08-13 09:58:35 +0800

    集成redisson实现分布式锁

--- springBoot_1/src/main/java/com/hhf/App.java
+++ springBoot_1/src/main/java/com/hhf/App.java
@@ -1,9 +1,13 @@
 package com.hhf;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -25,5 +29,15 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class,args);
 	}
+
+
+
+	//初始化redisson
+//	@Bean
+//	public Redisson redisson(){
+//		Config config=new Config();
+//		config.useSingleServer().setAddress("redis://www.hhf.com:6379").setPassword("feixiang").setDatabase(0);
+//		return (Redisson) Redisson.create(config);
+//	}
 	
 }
