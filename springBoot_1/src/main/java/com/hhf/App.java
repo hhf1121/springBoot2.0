617e1416f7a0e2caddf10200b2146566876c9f96
hhf <290071186@qq.com>
2019-08-15 17:27:56 +0800

    集成mongoDB，保存日志

--- springBoot_1/src/main/java/com/hhf/App.java
+++ springBoot_1/src/main/java/com/hhf/App.java
@@ -8,7 +8,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.text.DateFormat;
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Springboot全局启动app
@@ -39,5 +47,28 @@ public class App {
 //		config.useSingleServer().setAddress("redis://www.hhf.com:6379").setPassword("feixiang").setDatabase(0);
 //		return (Redisson) Redisson.create(config);
 //	}
+
+
+	/**
+	 * 请求参数，转换器:字符串->时间
+	 * @return
+	 */
+	@Bean
+	public Converter<String, Date> addNewConvert() {
+		return new Converter<String, Date>() {
+			@Override
+			public Date convert(String source) {
+				String replace = source.replace("T", " ");
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",Locale.US);
+				Date date = null;
+				try {
+					 date =sdf.parse(replace);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return date;
+			}
+		};
+	}
 	
 }
