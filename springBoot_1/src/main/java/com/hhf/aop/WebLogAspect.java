6449061bd47d9cb6c5ca364b33b8b1c17be31eb3
hehf <hehf@yimidida.com>
2019-10-12 15:31:26 +0800

    切面日志:mongodb->info

--- springBoot_1/src/main/java/com/hhf/aop/WebLogAspect.java
+++ springBoot_1/src/main/java/com/hhf/aop/WebLogAspect.java
@@ -27,12 +27,12 @@ import org.springframework.web.context.request.ServletRequestAttributes;
  *	更深层次：分布式日志收集（待续...）
  */
 
-//@Aspect
-//@Component
+@Aspect
+@Component
 public class WebLogAspect {
 
-	@Autowired
-	private MongoTemplate mongoTemplate;
+//	@Autowired
+//	private MongoTemplate mongoTemplate;
 
 	private Log log=null;
 
@@ -53,26 +53,26 @@ public class WebLogAspect {
 		log.setURL(request.getRequestURL().toString());
 		log.setIP(request.getRemoteAddr());
 		log.setHTTP_METHOD(request.getMethod());
-//		logger.info("URL:"+request.getRequestURL());
-//		logger.info("HTTP_METHOD:"+request.getMethod());
-//		logger.info("IP:"+request.getRemoteAddr());
+		logger.info("URL:"+request.getRequestURL());
+		logger.info("HTTP_METHOD:"+request.getMethod());
+		logger.info("IP:"+request.getRemoteAddr());
 		Enumeration<String> parameterNames = request.getParameterNames();
 		Map<String,String> params=new HashMap<String,String>();
 		while(parameterNames.hasMoreElements()) {
 			String name=parameterNames.nextElement();
 			params.put("name",name);
 			params.put("value",request.getParameter(name));
-//			logger.info("name:{},value:{}",name,request.getParameter(name));
+			logger.info("name:{},value:{}",name,request.getParameter(name));
 		}
 		log.setParams(params);
 	}
 	
 	@AfterReturning(returning="ret",pointcut="webLog()")
 	public void doAfterReturning(Object ret) {
-//		logger.info("RESPINSE:"+ret);
+		logger.info("RESPINSE:"+ret);
 		log.setRESPINSE(ret.toString());
 		log.setDate(new Date());
-		mongoTemplate.insert(log);
+//		mongoTemplate.insert(log);
 	}
 	
 }
