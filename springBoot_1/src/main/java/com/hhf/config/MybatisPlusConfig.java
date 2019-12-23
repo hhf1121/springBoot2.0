d26c8d78f5e29c8f1ca43b7ff3d723a87375a69f
hehf <hehf@yimidida.com>
2019-12-23 09:14:59 +0800

    mybatis-plus分页插件

---
+++ springBoot_1/src/main/java/com/hhf/config/MybatisPlusConfig.java
@@ -0,0 +1,33 @@
+package com.hhf.config;
+
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.core.MybatisXMLLanguageDriver;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.commons.lang3.StringUtils;
+//import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.io.IOException;
+
+/**
+ * @liuzongqiang
+ * @since 2019-07-16
+ */
+@EnableTransactionManagement
+@Configuration
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+}
